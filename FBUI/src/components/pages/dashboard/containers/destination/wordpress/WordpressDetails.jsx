3fa00d0315dd10b01b27b62edf74e7d05db3eba2
Anuragkg7dev <anuragkg7.dev@gmail.com>
2025-10-05 22:34:54 +0530

    Wordpress validation

--- FBUI/src/components/pages/dashboard/containers/destination/wordpress/WordpressDetails.jsx
+++ FBUI/src/components/pages/dashboard/containers/destination/wordpress/WordpressDetails.jsx
@@ -3,8 +3,18 @@ import { useState } from "react";
 import { BsFiletypeKey } from "react-icons/bs";
 import { FaRegUserCircle } from "react-icons/fa";
 import { SiCurl } from "react-icons/si";
+import { SOURCE_DESTINATION_KEY } from "../../ContainersConstant";
+import { DESTINATION_TYPE } from "@/components/client/EdgeConstant";
+import CustomLoaderButton from "@/components/common/element/CustomLoaderButton";
+import { verifyDestination } from "@/components/client/EdgeFunctionRepository";
+import { useAuthStore } from "@/components/store/AuthStateStore";
+import { toast } from "@/components/common/Notification";
+import { destinationValidationSchema } from "../../validation/ContainerValidation";
+import { validate } from "@/components/validation/ValidationUtil";
 
 export default function WordpressDetails(props) {
+
+    const { jwt: authkeyBearer } = useAuthStore();
 
     const id = props.id;
     const config = props.config;
@@ -12,18 +22,84 @@ export default function WordpressDetails(props) {
     const [user, setUser] = useState(config?.user ?? '')
     const [token, setToken] = useState(config?.token ?? '')
     const [url, setUrl] = useState(config?.url ?? '')
+    const [connected, setConnection] = useState(false)
+
+    const [loader, setLoader] = useState(false)
+
 
     const cml = props.cml;
     const cwidth = props.cwidth;
     const cvariant = props.variant ?? "fbloxD";
     const labelIconSize = props.labelIconSize
+
+
     const error = props.error
-    const validate = props.validate
+    const setError = props.setError;
+
+    const type = DESTINATION_TYPE.WORDPRESS
+
+    const connectAndVerify = () => {
+        if (!token || !url) {
+            toast.error('Token or URL missing')
+        } else {
+            setLoader(true)
+            const input = { token, url }
+            verifyDestination({ input, type }, verifyDestinationCallback, authkeyBearer)
+        }
+    }
+
+    const verifyDestinationCallback = (flag, result) => {
+        if (flag && result.flag) {
+            console.log(result)
+            setConnection(true)
+            toast.success('Test connection sucessful')
+        } else {
+            toast.error('Connection test failed')
+            setConnection(true)
+        }
+        setLoader(false)
+    }
+
+    const validateUrl = () => {
+        console.log(error)
+        console.log(setError)
+        console.log(url)
+        let urlError = validate(destinationValidationSchema, SOURCE_DESTINATION_KEY.URL, url)
+        setError({
+            ...error,
+            [SOURCE_DESTINATION_KEY.URL]: urlError
+        })
+        console.log(urlError)
+        return urlError
+    }
+
+
+
+    const validateUser = () => {
+        let userError = validate(destinationValidationSchema, SOURCE_DESTINATION_KEY.USER, user)
+        setError({
+            ...error,
+            [SOURCE_DESTINATION_KEY.USER]: userError
+        })
+        return userError
+
+    }
+
+    const validateToken = () => {
+        let tokenError = validate(destinationValidationSchema, SOURCE_DESTINATION_KEY.TOKEN, token)
+        setError({
+            ...error,
+            [SOURCE_DESTINATION_KEY.TOKEN]: tokenError
+        })
+        console.log(tokenError)
+        return tokenError
+
+    }
 
 
     return (
         <>
-            <Field.Root width="100%" color="brand.pureWhiteTxt" fontSize={{ base: "sm", md: "md" }} mb={6} invalid={error}>
+            <Field.Root width="100%" color="brand.pureWhiteTxt" fontSize={{ base: "sm", md: "md" }} mb={6} invalid={error?.[SOURCE_DESTINATION_KEY.URL]}>
                 <Field.Label>
                     <HStack>
                         <SiCurl size={labelIconSize} color="inherit" />
@@ -38,12 +114,12 @@ export default function WordpressDetails(props) {
                     value={url}
                     ml={cml}
                     width={cwidth}
-                    onBlur={() => validate?.()}
+                    onBlur={() => validateUrl()}
                 />
-                <Field.ErrorText ml={cml} fontSize={{ base: "xs", md: "sm" }}>{error}</Field.ErrorText>
+                <Field.ErrorText ml={cml} fontSize={{ base: "xs", md: "sm" }}>{error?.[SOURCE_DESTINATION_KEY.URL]}</Field.ErrorText>
             </Field.Root>
 
-            <Field.Root width="100%" color="brand.pureWhiteTxt" fontSize={{ base: "sm", md: "md" }} mb={6} invalid={error}>
+            <Field.Root width="100%" color="brand.pureWhiteTxt" fontSize={{ base: "sm", md: "md" }} mb={6} invalid={error?.[SOURCE_DESTINATION_KEY.USER]}>
                 <Field.Label>
                     <HStack>
                         <FaRegUserCircle size={labelIconSize} color="inherit" />
@@ -58,12 +134,12 @@ export default function WordpressDetails(props) {
                     value={user}
                     ml={cml}
                     width={cwidth}
-                    onBlur={() => validate?.()}
+                    onBlur={() => validateUser()}
                 />
-                <Field.ErrorText ml={cml} fontSize={{ base: "xs", md: "sm" }}>{error}</Field.ErrorText>
+                <Field.ErrorText ml={cml} fontSize={{ base: "xs", md: "sm" }}>{error?.[SOURCE_DESTINATION_KEY.USER]}</Field.ErrorText>
             </Field.Root>
 
-            <Field.Root width="100%" color="brand.pureWhiteTxt" fontSize={{ base: "sm", md: "md" }} mb={6} invalid={error}>
+            <Field.Root width="100%" color="brand.pureWhiteTxt" fontSize={{ base: "sm", md: "md" }} mb={6} invalid={error?.[SOURCE_DESTINATION_KEY.TOKEN]}>
                 <Field.Label>
                     <HStack>
                         <BsFiletypeKey size={labelIconSize} color="inherit" />
@@ -78,10 +154,19 @@ export default function WordpressDetails(props) {
                     value={token}
                     ml={cml}
                     width={cwidth}
-                    onBlur={() => validate?.()}
+                    onBlur={() => validateToken()}
                 />
-                <Field.ErrorText ml={cml} fontSize={{ base: "xs", md: "sm" }}>{error}</Field.ErrorText>
+                <Field.ErrorText ml={cml} fontSize={{ base: "xs", md: "sm" }}>{error?.[SOURCE_DESTINATION_KEY.TOKEN]}</Field.ErrorText>
             </Field.Root>
+
+            <CustomLoaderButton
+                cwidth="100%"
+                cvariant={"fbloxD"}
+                cloadingText={'Connecting'}
+                loader={loader}
+                onClickBtn={() => { connectAndVerify() }}
+                clabel={'Verify Integration'}
+            />
 
         </>
     );
